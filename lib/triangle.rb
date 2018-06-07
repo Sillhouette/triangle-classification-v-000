e90d2d22067ff28557d90a0d328da47d4e1c0bc6
Richard Melchior <raustin.melchior@gmail.com>
2018-06-07 03:17:23 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,19 +10,15 @@ class Triangle
   end
 
   def kind
-    puts "first check is #{@length_1} and #{@length_2} and #{@length_3}"
     a_and_b = @length_1 == @length_2
     a_and_c = @length_1 == @length_3
     b_and_c = @length_2 == @length_3
-    puts "second check is #{a_and_b} and #{a_and_c} and #{b_and_c}"
 
     triangle_inequality = (@length_1 + @length_2) > @length_3 &&
                           (@length_1 + @length_3) > @length_2 &&
                           (@length_2 + @length_3) > @length_1;
 
-    puts "third check is #{triangle_inequality}"
     check_length_validity = @length_1 <= 0 || @length_2 <= 0 || @length_3 <= 0;
-    puts "fourth check is #{check_length_validity}"
 
     if (check_length_validity) || !triangle_inequality
       raise TriangleError
