006d805465badd18cdb9f4ec32a79c07f730a20e
Richard Melchior <raustin.melchior@gmail.com>
2018-06-07 02:01:50 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,12 +8,21 @@ class Triangle
   end
 
   def kind
+
+    a_and_b = @length_1 == @length_2
+    a_and_c = @length_1 == @length_3
+    b_and_c = @length_2 == @length_3
+
+    triangle_inequality = @length_1 + @length_2 > @length_3 ||
+                          @length_1 + @length_3 > @length_2 ||
+                          @length_2 + @length_3 > @length_1;
+    
     if @length_1 > 0 && @length_2 > 0 && @length_3 > 0
-      equilateral = @length_1 == @length_2 && @length_2 == @length_3;
-      scalene = @length_1 != @length_2 && @length_2 != @length_3 && @length_1 != @length_3;
-      isosceles = (@length_1 == @length_2 && @length_1 != @length_3) ||
-                  (@length_1 == @length_3 && @length_1 != @length_2) ||
-                  (@length_2 == @length_3 && @length_1 != @length_2);
+      equilateral = a_and_b && b_and_c;
+      scalene = !a_and_b && !a_and_c && !b_and_c;
+      isosceles = (a_and_b && !a_and_c) ||
+                  (a_and_c && !a_and_b) ||
+                  (b_and_c && !a_and_b);
       if equilateral
         @equilateral
       elsif scalene
