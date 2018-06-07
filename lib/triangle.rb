f6a5505053fa4424864ceb0c22d7bf42dd598bef
Richard Melchior <raustin.melchior@gmail.com>
2018-06-07 01:40:38 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
-  # write code here
+  attr_accessor :type, :length_1, :length_2, :length_3, :equilateral, :scalene, :isosceles
+
+  def initialize(length_1, length_2,length_3)
+    @length_1 = length_1;
+    @length_2 = length_2;
+    @length_3 = length_3;
+  end
+
+  def kind
+    if @length_1 != 0 && @length_2 != 0 && @length_3 != 0 &&
+      equilateral = @length_1 == @length_2 == @length_3;
+      scalene = @length_1 != @length_2 != @length_3;
+      isosceles = (@length_1 == @length_2 && @length_1 != @length_3) ||
+                  (@length_1 == @length_3 && @length_1 != @length_2) ||
+                  (@length_2 == @length_3 && @length_1 != @length_2);
+      if equilateral
+        @equilateral
+      elsif scalene
+        @scalene
+      elsif isosceles
+        @isosceles
+      else
+        begin
+          raise TriangleError
+        rescue TriangleError => error
+          puts error.message;
+        end
+      end
+    end
+  end
+
+  class TriangleError
+    def message
+      "The given lengths are now a valid triangle."
+    end
+  end
+
 end
